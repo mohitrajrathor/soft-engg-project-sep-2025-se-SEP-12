71e845fab96e39bce717cea7e06af6ad580c30be
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2025-11-16 12:35:44 +0530

    slides generation service created

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -4,5 +4,6 @@ Services package for business logic.
 
 from .chatbot_service import ChatbotService
 from .quiz_service import QuizService
+from .slides_service import SlidesService, SlideTheme
 
-__all__ = ["ChatbotService", "QuizService"]
+__all__ = ["ChatbotService", "QuizService", "SlidesService", "SlideTheme"]
